b4bd669b480c0a21501516b79f2b1d57994b76b3
rahuljpZignuts <rahuljp@zignuts.com>
2022-01-12 17:22:29 +0530

    added Snackbar feature

--- app/src/main/java/com/example/testinggitgithub/MainActivity.kt
+++ app/src/main/java/com/example/testinggitgithub/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        snackbar()
+    }
+
+    private fun snackbar(){
+        // sample code for snackbar
     }
 }
